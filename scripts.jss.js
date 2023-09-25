552442db8cf66629b0e45c5e4fa808a2792e5579
Tina-muriu <97758217+Tina-muriu@users.noreply.github.com>
2023-09-25 14:59:55 +0300

    Add files via upload
    This repository demonstrates the integration of JavaScript with HTML and CSS to enhance web page styling. Explore dynamic content updates and various styling techniques to create engaging web interfaces

---
+++ scripts.jss.js
@@ -0,0 +1,6 @@
+    var coursesArray =["Angular" , "React" ,"Python","Vue","JavaScript","Ruby"]
+    var nextCourse="TypeScript";
+
+    window.onload=function showNextCourse(){
+        document.getElementById("futureCourses").innerHTML
+    }
